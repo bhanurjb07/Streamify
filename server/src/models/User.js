99279d9f25340624a1beb134e6f306b6d60ec28c
BHANUPRATAP SINGH <bhanurjb21@gmail.com>
2026-07-03 22:29:48 +0530

    resolve server  startup crashes and broken auth flow

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -45,6 +45,18 @@ const userSchema=new mongoose.Schema({
     }]
 }, {timestamps: true});
 
+serSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
+
+
 const User =mongoose.model("User", userSchema);
 
 export default User;
--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,29 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import 'dotenv/config';
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+import chatRoutes from './routes/chat.route.js';
 
 const app =express();
 const PORT=process.env.PORT || 4001;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req,res)=>{
     res.send("API Working..");
 })
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 ///Server and database connection
 const startServer=async ()=>{
